a1a238aa575a1bdc816236adf34e848bc66f41de
Joakim Przygoda Lesniewski <joakim.pl@hotmail.com>
2023-08-31 10:37:44 +0200

    övningar

---
+++ Lektion02/övning5.py
@@ -0,0 +1,11 @@
+print("ange fem heltal nedan!")
+
+a=int(input("Skriv här: "))
+b=int(input("Skriv här: "))
+c=int(input("Skriv här: "))
+e=int(input("Skriv här: "))
+d=int(input("Skriv här: "))
+
+
+x=max(a,b,c,e,d)
+print("Det högsta heltalet är",x)
---
+++ Lektion02/övning6.py
@@ -0,0 +1,41 @@
+
+
+vanligkorv2=int(input("2 vanliga korvar: "))
+vanligkorv3=int(input("3 vanliga korvar: "))
+
+veganskkorv2=int(input("2 veganska korvar: "))
+veganskkorv3=int(input("3 veganska korvar: "))
+
+vk2=vanligkorv2*2
+vk3=vanligkorv3*3
+x=vk2 + vk3
+vanligkorvpris= x/8*20.95
+vanligkorvpaket= x/8
+
+print("Priset för vanlig korv är",round(vanligkorvpris))
+print("Så många packet behöver ni",vanligkorvpaket,)
+print("Elever som vill ha 2 vanliga korvar",vanligkorv2)
+print("Elever som vill ha 3 vanliga korvar",vanligkorv3)
+
+vegkorv2=veganskkorv2*2
+vegkorv3=veganskkorv3*3
+y=vegkorv2+vegkorv3
+vegkorvpris=y/4*34.95
+vegkorvpaket=y/4
+
+print("Priset för vegansk korv är",round(vegkorvpris))
+print("Så många packet av veganska korvar behöver ni",vegkorvpaket,)
+print("Elever som vill ha 2 veganska korvar",veganskkorv2)
+print("Elever som vill ha 3 veganska korvar",veganskkorv3)
+
+dryck=vanligkorv2+vanligkorv3+veganskkorv2+veganskkorv3
+z=dryck*13.95
+
+print("Ni behöver",dryck,"drycker till eleverna")
+print("Det kommer och kosta",z,"kronor")
+print("Total kostnaden kommer att vara",vanligkorvpris+vegkorvpris+z)
+
+
+
+
+
