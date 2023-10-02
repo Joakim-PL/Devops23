2c9dc8486220ceabbdb3602bda31596c84ce7000
Joakim Przygoda Lesniewski <joakim.pl@hotmail.com>
2023-10-02 10:18:23 +0200

    projekt

--- G-nivå uppgift.py
+++ G-nivå uppgift.py
@@ -1,39 +1,56 @@
-import os
+import random
 
+class Hand:
+    def __init__(self):
+        self.korts = []
+
+    def add_card(self, card):
+        self.korts.append(card)
+
+    def calculate_score(self):
+        score = 0
+        esser = 0
+
+        for kort in self.korts:
+            if kort == "E":
+                score += 14
+                esser += 1
+            elif kort in ["Kn", "D", "K"]:
+                score += 10
+            else:
+                score += int(kort)
+
+        while score > 21 and esser > 0:
+            score -= 14
+            esser -= 1
+
+        return score
 
 class Kortlek:
-    def __init__(self):  # hämtar kortlek med färger och kort
-        self.farg = ["H", "K", "R", "S"]  # kortfärg,Hjärter,klöver,ruter,spader
-        self.kort = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Kn", "D", "K", "E"]
-        self.fullt_kortlek = []
+    def __init__(self):
+        self.farger = ["H", "K", "R", "S"]
+        self.varden = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Kn", "D", "K", "E"]
+        self.korts = []
 
-        for fargs in self.farg:
-            for korts in self.kort:
-                self.fullt_kortlek.append(fargs + korts)
+    def skapa_kortlek(self):
+        self.korts = [farg + vard for farg in self.farger for vard in self.varden]
 
-    def __str__(self):
-        hjarter = []
-        klover = []
-        ruter = []
-        spader = []
+    def blanda_kortlek(self):
+        random.shuffle(self.korts)
 
-        for fargs in self.farg:
-            for korts in self.kort:
-                if fargs == "H":
-                    hjarter.append(fargs + korts)
-                elif fargs == "K":
-                    klover.append(fargs + korts)
-                elif fargs == "R":
-                    ruter.append(fargs + korts)
-                else:
-                    spader.append(fargs + korts)
+    def dela_kort(self):
+        return self.korts.pop()
 
-        return ("Här är hela kortleken: \n\n"
-                f"Hjärter: {hjarter}\n"
-                f"Klöver: {klover}\n"
-                f"Ruter: {ruter}\n"
-                f"Spader: {spader}")
+def spel():
+    spelares_hand = Hand()
+    datorns_hand = Hand()
+    kortlek = Kortlek()
 
-    def blanda(self):  # blandar korleken funktion
-        kortlek_set = set(self.fullt_kortlek)
-        return list(kortlek_set)
+    print("=" * 40)
+    print("Välkommen".center(40))
+    print("Spelet Tjugoett".center(40))
+    print("Målet är att komma så nära 21 som möjligt".center(40))
+    print("Lycka till!".center(40))
+
+    kortlek.skapa_kortlek()
+    kortlek.blanda_kortlek()
